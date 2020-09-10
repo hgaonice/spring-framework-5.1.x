5a904e447c11ed9e1a6b2a46b2dcb4faaf8d2015
gaoh <5146513+gaohwh@user.noreply.gitee.com>
2020-09-10 17:50:47 +0800

    添加factorybean测试类

---
+++ spring-gaohwang-student/src/main/java/com/gaohwang/factorybean/config/Config.java
@@ -0,0 +1,15 @@
+package com.gaohwang.factorybean.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: gaoh
+ * @Date: 2020/9/8 17:48
+ * @Version: 1.0
+ */
+@Configuration
+@ComponentScan("com.gaohwang.factorybean")
+public class Config {
+}
---
+++ spring-gaohwang-student/src/main/java/com/gaohwang/factorybean/test/Test001.java
@@ -0,0 +1,30 @@
+package com.gaohwang.factorybean.test;
+
+import com.gaohwang.factorybean.config.Config;
+import com.gaohwang.factorybean.interfaces.CalculateService;
+import com.gaohwang.factorybean.interfaces.TestService;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @Description:
+ * @Author: gaoh
+ * @Date: 2020/9/8 17:53
+ * @Version: 1.0
+ */
+public class Test001 {
+
+    @Test
+    public void test() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        TestService testService = context.getBean(TestService.class);
+
+        CalculateService calculateService = context.getBean(CalculateService.class);
+
+
+        String testList = testService.getList("code123","name456");
+        String calculateResult = calculateService.getResult("测试");
+
+        System.out.println(testList + "," + calculateResult);
+    }
+}
