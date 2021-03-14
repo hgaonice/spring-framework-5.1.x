a5a691b2e6d8c6fc5446a03dfcb88535267155ce
gaoh <5146513+gaohwh@user.noreply.gitee.com>
2021-03-14 14:36:03 +0800

    DefaultSingletonBeanRegistry 添加注释

--- spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -67,11 +67,13 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 
 	/**
 	 * Cache of singleton factories: bean name to ObjectFactory.
+	 * 单例工厂的缓存：Bean名称为ObjectFactory
 	 */
 	private final Map<String, ObjectFactory<?>> singletonFactories = new HashMap<>(16);
 
 	/**
 	 * Cache of early singleton objects: bean name to bean instance.
+	 * 早期的单例对象的高速缓存：Bean名称到Bean实例。
 	 */
 	private final Map<String, Object> earlySingletonObjects = new HashMap<>(16);
 
